3db44a5b04b5a0b979203f3bc476409fb65d21a5
DESKTOP-M558PJJ\BRANKO <branko-96@hotmail.com>
2018-12-17 15:31:59 -0300

    v2.1
    config inicial
    
    Co-Authored-By: fedecarp <fedecarp@users.noreply.github.com>

---
+++ footer.php
@@ -0,0 +1,30 @@
+<footer class="footer">
+        <div class="container-fluid">
+          <nav class="float-left">
+            <ul>
+              <li>
+                <a href="#">
+                  LosPibesWeb
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <div class="copyright float-right">
+            &copy;
+            <script>
+              document.write(new Date().getFullYear())
+            </script>, made with <i class="material-icons">favorite</i> by
+            <a href="#" target="_blank">LosPibesWeb</a>
+          </div>
+          <!-- your footer here -->
+        </div>
+      </footer>
+
+  <!--   Core JS Files   -->
+  <script src="assets/js/core/jquery.min.js"></script>
+  <script src="assets/js/core/popper.min.js"></script>
+  <script src="assets/js/core/bootstrap-material-design.min.js"></script>
+  <script src="assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
+
+  <!-- Control Center for Material Dashboard: parallax effects, scripts for the example pages etc -->
+  <script src="assets/js/material-dashboard.js?v=2.1.1" type="text/javascript"></script>
---
+++ index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <title>Inicio</title>
+  <!-- Required meta tags -->
+  <?php include('header.php');?>
+</head>
+
+<body>
+  <div class="wrapper ">
+    <!-- MENU -->
+    <?php include('menu.php');?>
+      <!-- End Navbar -->
+      <div class="content">
+        <div class="container-fluid">
+          <!-- your content here -->
+          sdadsa
+        </div>
+      </div>
+      <!-- FOOTER -->
+      <?php include('footer.php');?>
+      <!-- END FOOTER -->
+    </div>
+  </div>
+
+<script src="js/index.js"></script>
+</body>
+
+</html>
