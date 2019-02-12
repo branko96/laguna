157cd064e09107a5bc0cc90e7c97e51ee58cfe33
branko <branko-96@hotmail.com>
2019-02-12 19:31:57 -0300

    Update lagunacampo.sql

--- BACKEND/database/lagunacampo.sql
+++ BACKEND/database/lagunacampo.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.4
--- https://www.phpmyadmin.net/
+-- version 4.1.14
+-- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 18-01-2019 a las 02:03:23
--- Versión del servidor: 5.7.14
--- Versión de PHP: 7.0.10
+-- Tiempo de generación: 12-02-2019 a las 23:30:25
+-- Versión del servidor: 5.6.17
+-- Versión de PHP: 5.5.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Base de datos: `lagunacampo`
@@ -26,15 +26,16 @@ SET time_zone = "+00:00";
 -- Estructura de tabla para la tabla `caravanas`
 --
 
-CREATE TABLE `caravanas` (
-  `id_caravana` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `caravanas` (
+  `id_caravana` int(11) NOT NULL AUTO_INCREMENT,
   `codigo` int(11) NOT NULL,
   `descripcion` varchar(25) NOT NULL,
   `peso` float NOT NULL,
   `sexo` varchar(20) NOT NULL,
   `categoria` varchar(25) NOT NULL,
-  `procedencia` varchar(20) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `procedencia` varchar(20) NOT NULL,
+  PRIMARY KEY (`id_caravana`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -42,10 +43,11 @@ CREATE TABLE `caravanas` (
 -- Estructura de tabla para la tabla `categorias`
 --
 
-CREATE TABLE `categorias` (
-  `id_categoria` int(11) NOT NULL,
-  `descripcion` varchar(25) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+CREATE TABLE IF NOT EXISTS `categorias` (
+  `id_categoria` int(11) NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(25) NOT NULL,
+  PRIMARY KEY (`id_categoria`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -53,8 +55,8 @@ CREATE TABLE `categorias` (
 -- Estructura de tabla para la tabla `empleados`
 --
 
-CREATE TABLE `empleados` (
-  `id_empleado` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `empleados` (
+  `id_empleado` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(25) NOT NULL,
   `apellido` varchar(25) NOT NULL,
   `puesto` varchar(25) NOT NULL,
@@ -63,8 +65,9 @@ CREATE TABLE `empleados` (
   `dni` int(20) NOT NULL,
   `cuil` int(25) NOT NULL,
   `cod_postal` int(15) NOT NULL,
-  `fecha_fin` date NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `fecha_fin` date NOT NULL,
+  PRIMARY KEY (`id_empleado`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
 
 --
 -- Volcado de datos para la tabla `empleados`
@@ -72,7 +75,13 @@ CREATE TABLE `empleados` (
 
 INSERT INTO `empleados` (`id_empleado`, `nombre`, `apellido`, `puesto`, `fecha_inicio`, `sueldo`, `dni`, `cuil`, `cod_postal`, `fecha_fin`) VALUES
 (1, 'Antonio', 'Lopez', 'peon', '2019-01-02', 15458.23, 17856987, 302566598, 8520, '2019-01-30'),
-(3, 'nuevo', 'edicion', 'peonero', '2019-01-15', 350.56, 124, 1234, 1212, '2019-01-03');
+(3, 'nuevo2', 'edicion2', 'peonero2', '2019-01-15', 352.56, 124, 1234, 212, '2019-01-02'),
+(5, 'nuevo', 'ott', 'peon', '2019-02-08', 21322, 34324433, 1233232, 8000, '0000-00-00'),
+(6, 'Branko', 'ottavianelli', 'dsfdfs', '2019-02-08', 324324, 21321231, 2313, 8000, '2019-02-06'),
+(7, 'Branko', 'ottavianelli', '123', '2019-02-08', 1232, 12321, 1232, 8000, '2019-02-05'),
+(8, 'b', '', '', '2019-02-08', 0, 0, 0, 0, '0000-00-00'),
+(9, 'Branko', 'ottavianelli', '3424', '2019-02-08', 423, 0, 0, 8000, '2019-02-05'),
+(10, 'Branko', 'ottavianelli', '3', '2019-02-12', 34, 34, 54354, 8000, '2019-02-04');
 
 -- --------------------------------------------------------
 
@@ -80,7 +89,7 @@ INSERT INTO `empleados` (`id_empleado`, `nombre`, `apellido`, `puesto`, `fecha_i
 -- Estructura de tabla para la tabla `movimientos`
 --
 
-CREATE TABLE `movimientos` (
+CREATE TABLE IF NOT EXISTS `movimientos` (
   `id_mov` int(11) NOT NULL,
   `id_caravana` int(11) NOT NULL,
   `fecha_mov` date NOT NULL,
@@ -94,64 +103,13 @@ CREATE TABLE `movimientos` (
 -- Estructura de tabla para la tabla `stock`
 --
 
-CREATE TABLE `stock` (
-  `id_stock` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `stock` (
+  `id_stock` int(11) NOT NULL AUTO_INCREMENT,
   `id_caravana` int(11) NOT NULL,
-  `total` int(11) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `total` int(11) NOT NULL,
+  PRIMARY KEY (`id_stock`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `caravanas`
---
-ALTER TABLE `caravanas`
-  ADD PRIMARY KEY (`id_caravana`);
-
---
--- Indices de la tabla `categorias`
---
-ALTER TABLE `categorias`
-  ADD PRIMARY KEY (`id_categoria`);
-
---
--- Indices de la tabla `empleados`
---
-ALTER TABLE `empleados`
-  ADD PRIMARY KEY (`id_empleado`);
-
---
--- Indices de la tabla `stock`
---
-ALTER TABLE `stock`
-  ADD PRIMARY KEY (`id_stock`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `caravanas`
---
-ALTER TABLE `caravanas`
-  MODIFY `id_caravana` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT de la tabla `categorias`
---
-ALTER TABLE `categorias`
-  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT de la tabla `empleados`
---
-ALTER TABLE `empleados`
-  MODIFY `id_empleado` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
---
--- AUTO_INCREMENT de la tabla `stock`
---
-ALTER TABLE `stock`
-  MODIFY `id_stock` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
