bb1d22160e6198d25b6172fe10b6a88b7a089c91
fedecarp <45774031+fedecarp@users.noreply.github.com>
2019-02-20 20:26:09 -0300

    Update lagunacampo.sql

--- BACKEND/database/lagunacampo.sql
+++ BACKEND/database/lagunacampo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 12-02-2019 a las 23:57:19
+-- Tiempo de generación: 21-02-2019 a las 00:22:47
 -- Versión del servidor: 10.1.37-MariaDB
 -- Versión de PHP: 7.3.1
 
@@ -32,11 +32,20 @@ CREATE TABLE `caravanas` (
   `id_caravana` int(11) NOT NULL,
   `codigo` int(11) NOT NULL,
   `descripcion` varchar(25) NOT NULL,
-  `peso` float NOT NULL,
+  `peso` varchar(25) NOT NULL,
   `sexo` varchar(20) NOT NULL,
   `categoria` varchar(25) NOT NULL,
   `procedencia` varchar(20) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `caravanas`
+--
+
+INSERT INTO `caravanas` (`id_caravana`, `codigo`, `descripcion`, `peso`, `sexo`, `categoria`, `procedencia`) VALUES
+(2, 55, 'caballo', '124', 'femenino', 'potrillo', 'chubut'),
+(3, 45, 'prueba', '123', 'masculino', 'novillo', 'bs as'),
+(4, 12, 'vaca grande', '2345', 'femenino', 'adulta', 'neuquen');
 
 -- --------------------------------------------------------
 
@@ -74,14 +83,14 @@ CREATE TABLE `empleados` (
 --
 
 INSERT INTO `empleados` (`id_empleado`, `nombre`, `apellido`, `puesto`, `fecha_inicio`, `sueldo`, `email`, `dni`, `cuil`, `cod_postal`, `fecha_fin`) VALUES
-(1, 'Antonio', 'Lopez', 'peon', '2019-01-02', 15458.23, '', 17856987, 302566598, 8520, '2019-01-30'),
-(3, 'nuevo2', 'edicion2', 'peonero2', '2019-01-15', 352.56, '', 124, 1234, 212, '2019-01-02'),
-(5, 'nuevo', 'ott', 'peon', '2019-02-08', 21322, '', 34324433, 1233232, 8000, '0000-00-00'),
+(3, 'nuevo', 'edicion', 'peonero', '2019-01-15', 350.56, 'fefefe@hotmail.com', 124, 1234, 1212, '2019-01-03'),
+(12, 'Juan', 'Moran', 'empleado', '2019-02-17', 45234, 'alfre.89@hotmail.com', 3538934, 203414312, 8000, '0000-00-00'),
 (6, 'Branko', 'ottavianelli', 'dsfdfs', '2019-02-08', 324324, '', 21321231, 2313, 8000, '2019-02-06'),
 (7, 'Branko', 'ottavianelli', '123', '2019-02-08', 1232, '', 12321, 1232, 8000, '2019-02-05'),
-(8, 'b', '', '', '2019-02-08', 0, '', 0, 0, 0, '0000-00-00'),
 (9, 'Branko', 'ottavianelli', '3424', '2019-02-08', 423, '', 0, 0, 8000, '2019-02-05'),
-(10, 'Branko', 'ottavianelli', '3', '2019-02-12', 34, '', 34, 54354, 8000, '2019-02-04');
+(10, 'Branko', 'ottavianelli', '3', '2019-02-12', 34, '', 34, 54354, 8000, '2019-02-04'),
+(11, 'asd', 'fwfw', 'adad', '2019-02-13', 123.4, 'fede@hotmail.com', 124, 1234, 123, '2019-01-02'),
+(13, 'Juan', 'Moran', 'empleado', '2019-02-17', 45234, 'alfre.89@hotmail.com', 3538934, 203414312, 8000, '2019-02-19');
 
 -- --------------------------------------------------------
 
@@ -108,6 +117,25 @@ CREATE TABLE `stock` (
   `id_caravana` int(11) NOT NULL,
   `total` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ventas`
+--
+
+CREATE TABLE `ventas` (
+  `id_ventas` int(11) NOT NULL,
+  `fecha` date NOT NULL,
+  `num_fact` int(25) NOT NULL,
+  `cabezas` int(10) NOT NULL,
+  `kg` float NOT NULL,
+  `peso_x_kg` float NOT NULL,
+  `bruto` float NOT NULL,
+  `IVA` float NOT NULL,
+  `neto` float NOT NULL,
+  `retencion` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Índices para tablas volcadas
@@ -138,6 +166,12 @@ ALTER TABLE `stock`
   ADD PRIMARY KEY (`id_stock`);
 
 --
+-- Indices de la tabla `ventas`
+--
+ALTER TABLE `ventas`
+  ADD PRIMARY KEY (`id_ventas`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -145,7 +179,7 @@ ALTER TABLE `stock`
 -- AUTO_INCREMENT de la tabla `caravanas`
 --
 ALTER TABLE `caravanas`
-  MODIFY `id_caravana` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_caravana` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `categorias`
@@ -157,13 +191,19 @@ ALTER TABLE `categorias`
 -- AUTO_INCREMENT de la tabla `empleados`
 --
 ALTER TABLE `empleados`
-  MODIFY `id_empleado` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_empleado` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT de la tabla `stock`
 --
 ALTER TABLE `stock`
   MODIFY `id_stock` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `ventas`
+--
+ALTER TABLE `ventas`
+  MODIFY `id_ventas` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
