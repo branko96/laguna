1a9aa16dae7ce320a84100b8deac922e4d1324f5
branko <branko-96@hotmail.com>
2019-03-05 12:59:08 -0300

    ventas

--- ventas.php
+++ ventas.php
@@ -20,10 +20,10 @@
                 <div class="card-header card-header-warning">
                   <div class="row">
                     <div class="col-md-6">
-                      <h4 class="card-title">Empleados</h4>
+                      <h4 class="card-title">Caravanas</h4>
                     </div>
                     <div class="col-md-6 text-right">
-                      <a href="#" id="nuevo_empleado" data-target="#modal_nuevo_user" data-toggle="modal" class="btn btn-success btn-round">Nuevo<div class="ripple-container"></div></a>
+                      <a href="#" data-target="#modal_nueva_caravana" data-toggle="modal" class="btn btn-success btn-round">Nuevo<div class="ripple-container"></div></a>
                     </div>
                   </div>
 
@@ -32,34 +32,28 @@
                   <table class="table table-hover">
                     <thead class="text-warning">
                       <th>ID</th>
-                      <th>Nombre</th>
-                      <th>Apellido</th>
-                      <th>DNI</th>
-                      <th>Cuil</th>
-                      <th>Codigo Postal</th>
-                      <th>Puesto</th>
-                      <th>Sueldo</th>
-                      <th>Fecha Inicio</th>
-                      <th>Fecha Fin</th> 
+                      <th>Codigo</th>
+                      <th>Descripcion</th>
+                      <th>Peso</th>
+                      <th>Sexo</th>
+                      <th>Categoria</th>
+                      <th>Procedencia</th>
                       <th>Accion</th>
                     </thead>
                     <tbody>
-                      <tr v-for="empleado in empleados">
-                        <td>{{empleado.id}}</td>
-                        <td>{{empleado.nombre}}</td>
-                        <td>{{empleado.apellido}}</td>
-                        <td>{{empleado.dni}}</td>
-                        <td>{{empleado.cuil}}</td>
-                        <td>{{empleado.cod_postal}}</td>
-                        <td>{{empleado.puesto}}</td>
-                        <td>{{empleado.sueldo}}</td>
-                        <td>{{empleado.fecha_inicio}}</td>
-                        <td>{{empleado.fecha_fin}}</td>
+                      <tr v-for="caravana in caravanas">
+                        <td>{{caravana.id}}</td>
+                        <td>{{caravana.codigo}}</td>
+                        <td>{{caravana.descripcion}}</td>
+                        <td>{{caravana.peso}}</td>
+                        <td>{{caravana.sexo}}</td>
+                        <td>{{caravana.categoria}}</td>
+                        <td>{{caravana.procedencia}}</td>
                         <td class="td-actions text-center">
-                          <button type="button" title="Editar" @click="modal_editar(empleado.id);" class="btn btn-primary btn-link btn-sm">
+                          <button type="button" title="Editar" @click="modal_editar(caravana);" class="btn btn-primary btn-link btn-sm">
                             <i class="material-icons">edit</i>
                           </button>
-                          <button type="button" @click="eliminar_empleado(empleado.id)" title="Borrar" class="btn btn-danger btn-link btn-sm">
+                          <button type="button" @click="eliminar_caravana(caravana.id)" title="Borrar" class="btn btn-danger btn-link btn-sm">
                             <i class="material-icons">close</i>
                           </button>
                         </td>
@@ -72,7 +66,7 @@
         </div>
 
 
-<div class="modal fade" id="modal_editar_user">
+<div class="modal fade" id="modal_editar_caravana">
   <div class="modal-dialog modal-lg modal-dialog-centered">
     <div class="modal-content">
 
@@ -88,7 +82,7 @@
                 <div class="card-header card-header-success">
                   <div class="row">
                     <div class="col-md-10">
-                      <h4 class="card-title">Editar Empleado</h4>
+                      <h4 class="card-title">Editar Caravana</h4>
                     </div>
                     <div class="col-md-2">
                       <button type="button" class="close" data-dismiss="modal">&times;</button>
@@ -98,72 +92,46 @@
                   
                 </div>
                 <div class="card-body">
-                  <form @submit.prevent="editar_empleado">
-                    <div class="row">
+                  <form @submit.prevent="editar_caravana">
+                     <div class="row">
                       <div class="col-md-5">
                         <div class="form-group">
-                          <label class="">Nombre</label>
-                          <input type="text" v-model="empleado_editar.nombre" name="nombre" class="form-control">
+                          <label class="">Código</label>
+                          <input type="text" name="nombre" v-model="caravana_editar.codigo" class="form-control">
                         </div>
                       </div>
                       <div class="col-md-3">
                         <div class="form-group">
-                          <label class="">Apellido</label>
-                          <input type="text" v-model="empleado_editar.apellido" class="form-control">
+                          <label class="">Descripción</label>
+                          <input type="text" v-model="caravana_editar.descripcion" class="form-control">
                         </div>
                       </div>
                       <div class="col-md-4">
                         <div class="form-group">
-                          <label class="">Dni</label>
-                          <input type="text" v-model="empleado_editar.dni" max-length="10" class="form-control">
+                          <label class="">Peso</label>
+                          <input type="text" v-model="caravana_editar.peso" max-length="10" class="form-control">
                         </div>
                       </div>
                     </div>
                     <div class="row">
                       <div class="col-md-6">
                         <div class="form-group">
-                          <label class="">Email</label>
-                          <input type="email" v-model="empleado_editar.email" class="form-control">
+                          <label class="">Sexo</label>
+                          <input type="text" v-model="caravana_editar.sexo" class="form-control">
                         </div>
                       </div>
                       <div class="col-md-6">
                         <div class="form-group">
-                          <label class="">Cuil</label>
-                          <input type="text" v-model="empleado_editar.cuil" class="form-control">
+                          <label class="">Categoría</label>
+                          <input type="text" v-model="caravana_editar.categoria" class="form-control">
                         </div>
                       </div>
                     </div>
                     <div class="row">
                       <div class="col-md-6">
                         <div class="form-group">
-                          <label class="">Puesto</label>
-                          <input type="text" v-model="empleado_editar.puesto" class="form-control">
-                        </div>
-                      </div>
-                      <div class="col-md-6">
-                        <div class="form-group">
-                          <label class="">Sueldo</label>
-                          <input type="text" v-model="empleado_editar.sueldo" class="form-control">
-                        </div>
-                      </div>
-                    </div>
-                    <div class="row">
-                      <div class="col-md-4">
-                        <div class="form-group">
-                          <label class="">Fecha Inicio</label>
-                          <input type="text" disabled v-model="empleado_editar.fecha_inicio" class="form-control">
-                        </div>
-                      </div>
-                      <div class="col-md-4">
-                        <div class="form-group">
-                          <label class="">Fecha Fin</label>
-                          <input type="text" v-model="empleado_editar.fecha_fin" class="form-control">
-                        </div>
-                      </div>
-                      <div class="col-md-4">
-                        <div class="form-group">
-                          <label class="">Codigo Postal</label>
-                          <input type="text" v-model="empleado_editar.cod_postal" class="form-control">
+                          <label class="">Procedencia</label>
+                          <input type="text" v-model="caravana_editar.procedencia" class="form-control">
                         </div>
                       </div>
                     </div>
@@ -189,7 +157,7 @@
   </div>
 </div>
 <!-- The Modal -->
-<div class="modal fade" id="modal_nuevo_user">
+<div class="modal fade" id="modal_nueva_caravana">
   <div class="modal-dialog modal-lg modal-dialog-centered">
     <div class="modal-content">
 
@@ -205,7 +173,7 @@
                 <div class="card-header card-header-success">
                   <div class="row">
                     <div class="col-md-10">
-                      <h4 class="card-title">Nuevo Empleado</h4>
+                      <h4 class="card-title">Nueva Caravana</h4>
                     </div>
                     <div class="col-md-2">
                       <button type="button" class="close" data-dismiss="modal">&times;</button>
@@ -215,80 +183,50 @@
                   
                 </div>
                 <div class="card-body">
-                  <form @submit.prevent="nuevo_empleado">
+                  <form @submit.prevent="nueva_caravana">
                     <div class="row">
                       <div class="col-md-5">
                         <div class="form-group">
-                          <label class="bmd-label-floating">Nombre</label>
-                          <input type="text" name="nombre" v-model="nuevo_emp.nombre" class="form-control">
+                          <label class="bmd-label-floating">Código</label>
+                          <input type="text" name="nombre" v-model="nuev_caravana.codigo" class="form-control">
                         </div>
                       </div>
                       <div class="col-md-3">
                         <div class="form-group">
-                          <label class="bmd-label-floating">Apellido</label>
-                          <input type="text" v-model="nuevo_emp.apellido" class="form-control">
+                          <label class="bmd-label-floating">Descripción</label>
+                          <input type="text" v-model="nuev_caravana.descripcion" class="form-control">
                         </div>
                       </div>
                       <div class="col-md-4">
                         <div class="form-group">
-                          <label class="bmd-label-floating">Dni</label>
-                          <input type="text" v-model="nuevo_emp.dni" max-length="10" class="form-control">
+                          <label class="bmd-label-floating">Peso</label>
+                          <input type="text" v-model="nuev_caravana.peso" max-length="10" class="form-control">
                         </div>
                       </div>
                     </div>
                     <div class="row">
                       <div class="col-md-6">
                         <div class="form-group">
-                          <label class="bmd-label-floating">Email</label>
-                          <input type="email" v-model="nuevo_emp.email" class="form-control">
+                          <label class="bmd-label-floating">Sexo</label>
+                          <input type="text" v-model="nuev_caravana.sexo" class="form-control">
                         </div>
                       </div>
                       <div class="col-md-6">
                         <div class="form-group">
-                          <label class="bmd-label-floating">Cuil</label>
-                          <input type="text" v-model="nuevo_emp.cuil" class="form-control">
+                          <label class="bmd-label-floating">Categoría</label>
+                          <input type="text" v-model="nuev_caravana.categoria" class="form-control">
                         </div>
                       </div>
                     </div>
                     <div class="row">
                       <div class="col-md-6">
                         <div class="form-group">
-                          <label class="bmd-label-floating">Puesto</label>
-                          <input type="text" v-model="nuevo_emp.puesto" class="form-control">
-                        </div>
-                      </div>
-                      <div class="col-md-6">
-                        <div class="form-group">
-                          <label class="bmd-label-floating">Sueldo</label>
-                          <input type="text" v-model="nuevo_emp.sueldo" class="form-control">
+                          <label class="bmd-label-floating">Procedencia</label>
+                          <input type="text" v-model="nuev_caravana.procedencia" class="form-control">
                         </div>
                       </div>
                     </div>
-                    <div class="row">
-                      <!-- <div class="col-md-4">
-                        <div class="form-group">
-                          <label class="bmd-label-floating">Fecha Fin</label>
-                          <input type="date" v-model="nuevo_emp.fecha_fin" class="form-control">
-                        </div>
-                      </div> -->
-                      <div class="col-md-6">
-                        <div class="form-group">
-                          <label class="bmd-label-floating">Codigo Postal</label>
-                          <input type="text" v-model="nuevo_emp.cod_postal" class="form-control">
-                        </div>
-                      </div>
-                    </div>
-                   <!--  <div class="row">
-                      <div class="col-md-12">
-                        <div class="form-group">
-                          <label>About Me</label>
-                          <div class="form-group">
-                            <label class="bmd-label-floating"> Lamborghini Mercy, Your chick she so thirsty, I'm in that two seat Lambo.</label>
-                            <textarea class="form-control" rows="5"></textarea>
-                          </div>
-                        </div>
-                      </div>
-                    </div> -->
+                    
                     <button type="submit" class="btn btn-success pull-right">Guardar</button>
                     <button type="button" class="btn btn-danger" data-dismiss="modal">Cerrar</button>
                     <div class="clearfix"></div>
@@ -320,7 +258,7 @@
 <script src="js/vue-axios.min.js"></script>
 <script src="js/index.js"></script>
 <script src="assets/js/plugins/bootstrap-notify.js"></script>
-<script src="js/empleados-controller.js"></script>
+<script src="js/ventas-controller.js"></script>
 </body>
 
 </html>
